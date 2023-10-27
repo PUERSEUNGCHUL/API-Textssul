f987c12f95404c28b48d3091e6babb7ace737884
puer <lhhl1029@gmail.com>
2023-10-28 01:16:49 +0900

    뼈대

---
+++ src/main/java/kr/co/puerpuella/apitextssul/ApiTextSsulApplication.java
@@ -0,0 +1,13 @@
+package kr.co.puerpuella.apitextssul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTextSsulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTextSsulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/puerpuella/apitextssul/ApiTextSsulApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.puerpuella.apitextssul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTextSsulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
